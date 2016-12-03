f26962e9f6335ed8dca7572d9ed22a4386ebed88
mkg20001 <mkg20001@gmail.com>
2016-12-03 10:42:01 +0100

    Update

--- scripts/launcher.py
+++ scripts/launcher.py
@@ -6,21 +6,26 @@ import os
 import sys
 
 # ZeroNet Modules
-import imp
-
-zeronet = imp.load_source('zeronet', os.environ['SNAP']+'/zeronet.py') #todo: fix snapcraft.yml and change back to "import zeronet"
+import zeronet
 
 def mkdirp(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+def setarg(arg,val):
+    if arg not in sys.argv:
+        sys.argv = [sys.argv[0]]+[arg,val]+sys.argv[1:]
+
 def main():
     print("- Please report snap specific errors (e.g. Read-only file system) to: https://github.com/mkg20001/zeronet-snap/issues")
-    sys.argv = [sys.argv[0]]+["--data_dir", os.environ['SNAP_USER_COMMON']+"/data", "--config_file", os.environ['SNAP_USER_COMMON']+"/zeronet.conf", "--log_dir", os.environ['SNAP_USER_DATA']+"/log"]+sys.argv[1:]
+    print("- and ZeroNet specific errors to: https://github.com/HelloZeroNet/ZeroNet/issues")
+    setarg("--data_dir",os.environ['SNAP_USER_COMMON']+"/data")
+    setarg("--config_file", os.environ['SNAP_USER_COMMON']+"/zeronet.conf")
+    setarg("--log_dir", os.environ['SNAP_USER_DATA']+"/log")
     mkdirp(os.environ['SNAP_USER_COMMON']+"/data")
     mkdirp(os.environ['SNAP_USER_DATA']+"/log")
     os.chdir(os.environ['SNAP'])
-    zeronet.main()
+    sys.exit(zeronet.main())
 
 if __name__ == '__main__':
     main()
